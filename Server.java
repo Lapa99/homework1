820bd912cb9770813f193a37a5e0d6adf834313a
Artem <ph.lapa@yandex.ru>
2020-12-15 17:49:28 +0300

    Server class

---
+++ Server.java
@@ -0,0 +1,18 @@
+public class Server {
+    private Vector<ClientHandler> clients;
+    private int counter;
+    public Server() {
+        counter = 0;
+        try {
+            ServerSocket serverSocket = new ServerSocket(8289);
+            clients = new Vector<>();
+            while (true) {
+                System.out.println("Ждём подключения клиента");
+                Socket socket = serverSocket.accept();
+                ClientHandler c = new ClientHandler(this, socket, "Клиент #" + ++counter);
+                subscribe(c);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
