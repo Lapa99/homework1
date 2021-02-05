4eeeedf16c1784ef0a2eb6bba2b8f93835b770b0
Artem <ph.lapa@yandex.ru>
2021-02-05 17:41:09 +0300

    Lesson6Task2

---
+++ Lesson6Task2.java
@@ -0,0 +1,11 @@
+public class Lesson6Task2 {
+    public static boolean FourAndOne(int[] arr) {
+        boolean i1 = false, i4 = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) i1 = true;
+            else if (arr[i] == 4) i4 = true;
+            else return false;
+        }
+        return i1 && i4;
+    }
+}
